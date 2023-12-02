8539e7bce8ac4999105dc610c295f943d4af97c1
Ankush Shekhawat <ankush75056@gmail.com>
2023-12-02 13:33:53 +0530

    day 3 complete

---
+++ functionality.js
@@ -0,0 +1,131 @@
+let activeCellId = null;
+const activeCellElement =document.getElementById("active-cell");
+const form = document.querySelector(".form");
+const state ={};
+
+form.addEventListener("change", onChangeFormData)
+
+const defaultStyles ={
+    //Todo: change it later
+    fontFamily: "poppins-regular",
+    fontSize: 16,
+    isBold: false,
+    isItalic: false,
+    isUnderline: false,
+    align: "left",
+    textColor: "#00000",
+    bgColor: "#ffffff",
+    text: ""
+}
+
+function onChangeCellText(event){
+    let changedText = event.target.innerText;
+    if(state[activeCellId]){
+        // the current cell is already added to state object
+        state[activeCellId].text = changedText;
+    }
+    else{
+        state[activeCellId] = {...defaultStyles, text: changedText};
+    }
+}
+
+function onChangeFormData(){
+    const options = {
+        fontFamily: form["fontFamily"].value,
+        fontSize: form["fontSize"].value,
+        isBold: form["isBold"].checked,
+        isItalic: form["isItalic"].checked,
+        // isUnderLine: form["isUnderline"].checked,
+        isUnderline: form.isUnderline.checked,
+        align: form.align.value,  // "left" | "center" | "right"
+        textColor: form["textColor"].value,
+        bgColor: form["bgColor"].value,
+
+
+    };
+    applyStyles(options);
+}
+
+function applyStyles(styles){
+    // it will apply the styles to th active cell
+    if(!activeCellId){
+
+        form.reset();
+        alert("Please select cell to apply");
+        return;
+    }
+  
+
+    // if some cell is selected than apply styles to that cell
+    const activeCell = document.getElementById(activeCellId);
+    activeCell.style.color = styles.textColor;
+    activeCell.style.backgroundColor = styles.bgColor;
+    activeCell.style.textAlign = styles.align;
+    activeCell.style.fontWeight = styles.isBold ? "600" : "400";
+    activeCell.style.fontFamily = styles.fontFamily;
+    activeCell.style.fontSize = styles.fontSize + "px";
+    activeCell.style.textDecoration = styles.isUnderline ? "underline" : "none";
+    activeCell.style.fontStyle = styles.isItalic ? "italic" : "normal";
+
+    // whenever there's an update in a cell style, update these style with the state object.
+    state[activeCellId] = {...styles, text:activeCell.innerText};
+}
+
+function onFocusCell(event){
+    if(activeCellId == event.target.id) return;
+    activeCellId = event.target.id;
+    activeCellElement.innerText = activeCellId;
+
+
+    // reset the form with it actual style
+    if(state[activeCellId]){
+        // already touched cell
+        resetForm(state[activeCellId]);
+    }
+    else{
+        resetForm(defaultStyles);
+    }
+
+} 
+
+function resetForm(styles){
+/** 
+    styles  = {
+        fontFamily: form["fontFamily"].value,
+        fontSize: form["fontSize"].value + "px",
+        isBold: form["isBold"].checked,
+        isItalic: form["isItalic"].checked,
+        // isUnderLine: form["isUnderline"].checked,
+        isUnderline: form.isUnderline.checked,
+        align: form.align.value,  // "left" | "center" | "right"
+        textColor: form["textColor"].value,
+        bgColor: form["bgColor"].value
+    };
+    */
+
+    console.log(styles);
+
+    form.fontSize.value = styles.fontSize;
+    form.fontFamily.value = styles.fontFamily;
+    form.isBold.checked = styles.isBold;
+    form.isItalic.checked = styles.isItalic;
+    form.isUnderline.checked = styles.isUnderline;
+    form.align.value = styles.align;
+    form.textColor.value = styles.textColor;
+    form.bgColor.value = styles.bgColor;
+}
+
+
+function exportData(){
+    // Todo: export the file data and download it
+    //{a:10, b:20} => '{"a": 10, "b":20}'  //stringfy method reference
+   const jsonData = JSON.stringify(state);
+   console.log(jsonData);
+   const blob = new Blob([jsonData], {type: "text/plain"});
+
+   const url = URL.createObjectURL(blob);
+   const link = document.createElement("a");
+   link.download = "data.json";
+   link.href = url;
+   link.click();
+}
